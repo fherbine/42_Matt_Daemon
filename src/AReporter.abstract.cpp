13f062d9c784c5a5af8a4976c4b5b26f4352f4d4
fherbine <felix.herbinet@yahoo.com>
2022-09-29 12:18:25 +0200

    must be root fix

--- src/AReporter.abstract.cpp
+++ src/AReporter.abstract.cpp
@@ -18,6 +18,11 @@ AReporter::AReporter(std::ostream & outputStream, log_level_t logLevel) : _logLe
 
 AReporter::AReporter(std::string logfile, log_level_t logLevel) : _logLevel(logLevel) {
 
+    if (!Utility::OS::amIRoot()) {
+        std::cerr << "Must be root !" << std::endl;
+        exit(-1);
+    }
+
     if (!std::filesystem::exists(std::filesystem::path(logfile).parent_path()))
         Utility::OS::Path::create_folder_tree(std::filesystem::path(logfile).parent_path());
         
